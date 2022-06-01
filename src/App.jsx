bdaa61cde574bdb063348d81d59f5ddfceb45378
ruairidhflint <roryflintphoto@gmail.com>
2022-06-01 09:14:02 +0100

    Implement full screen toggle - focus state is removed from text editor so must add back

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState, useCallback } from 'react'
+import { FullScreen, useFullScreenHandle } from 'react-full-screen'
 
 import useLocalStorage from './hooks/useLocalStorage'
 import TextInput from './components/text-input'
 import Settings from './components/settings'
 import Help from './components/help'
+import FullScreenButton from './components/full-screen'
 import downloadFile from './utilities/download'
 import './App.css'
 
 function App() {
+  const handle = useFullScreenHandle()
   const [settings, setSettings] = useLocalStorage('settings', {
     darkmode: false,
     font: 'serif',
@@ -39,23 +42,30 @@ function App() {
   }, [])
 
   return (
-    <div className="App" data-theme={settings.darkmode && 'dark'}>
-      <Help visible={showHelp} setVisible={setShowHelp} />
-      <Settings
-        settings={settings}
-        setSettings={setSettings}
-        visible={showSettings}
-        setVisible={setShowSettings}
-        download={save}
-      />
-      <TextInput
-        text={text}
-        setText={setText}
-        settings={settings}
-        settingsModal={showSettings}
-        helpModal={showHelp}
-      />
-    </div>
+    <FullScreen handle={handle}>
+      <div className="App" data-theme={settings.darkmode && 'dark'}>
+        <FullScreenButton
+          active={handle.active}
+          enter={handle.enter}
+          exit={handle.exit}
+        />
+        <Help visible={showHelp} setVisible={setShowHelp} />
+        <Settings
+          settings={settings}
+          setSettings={setSettings}
+          visible={showSettings}
+          setVisible={setShowSettings}
+          download={save}
+        />
+        <TextInput
+          text={text}
+          setText={setText}
+          settings={settings}
+          settingsModal={showSettings}
+          helpModal={showHelp}
+        />
+      </div>
+    </FullScreen>
   )
 }
 
